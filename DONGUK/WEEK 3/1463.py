32ae613c486fff1f3c5d7dbfe85c67df6fce9d15
kim0914 <kosa0914@naver.com>
2022-03-22 22:05:12 +0900

    03.22

---
+++ DONGUK/WEEK 3/1463.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+dp = [0] * 1000001
+dp[1] = 0
+dp[2] = 1
+dp[3] = 1
+
+
+for i in range(4, 1000000):
+    nums = []
+    nums.append(i-1)
+    if i % 3 == 0:
+        nums.append(i//3)
+    
+    if i % 2 == 0:
+        nums.append(i//2)
+    
+    vals = []
+    for num in nums:
+        vals.append(dp[num])
+    
+    dp[i] = min(vals) + 1
+
+
+print(dp[N])
+
+'''
+10 -> 5 -> 4 -> 2
+'''
