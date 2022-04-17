29f9f427e8da706b264d0d7a8f7f91532725d375
kim0914 <kosa0914@naver.com>
2022-04-17 15:34:32 +0900

    04.14

--- DONGUK/WEEK 6/6549.py
+++ DONGUK/WEEK 6/6549.py
@@ -1,48 +1,7 @@
 import sys
+from collections import deque
+
 input = sys.stdin.readline
-
-def calc(data):
-    N = data[0]
-    dp = [0] * (N + 1)
-
-    same_area = 0
-    same_flag = False
-    diff_cnt = 1
-    for i in range(1, N+1):
-        if i == 1:
-            dp[i] = data[1]
-
-        else:
-            diff = data[i] - data[i-1]
-            if data[i-1] < data[i]: # 이전의 수 보다 큰 경우
-                same_flag = False
-                if diff == 1:
-                    diff_cnt += 1
-                    
-                
-                else:
-                    diff_cnt = 0
-                    dp[i] = max(data[i] , data[i-1] * 2)
-
-            
-            elif data[i-1] == data[i]: # 같은 경우
-                if same_flag == False:
-                    same_area = data[i-1]
-                    same_flag = True
-
-                dp[i] = dp[i-1] + same_area
-            
-            else: # 작은 경우
-                same_flag = False
-                if diff == -1:
-                    diff_cnt += 1
-                    dp[i] = max(dp[i-1], data[i] * diff_cnt)
-            
-                else:
-                    diff_cnt = 0
-                    dp[i] = data[i] * 2
-    print(dp)
-    return max(dp)
 
 
 while True:
@@ -51,22 +10,35 @@ while True:
     if len(data) == 1 and data[0] == 0:
         break
     
-    print(calc(data))
+    N = data.pop(0)
+    d = deque()
+    ans = 0
+    for i in range(N):
+        while len(d) > 0 and data[d[-1]] > data[i]:
+            tmp = d.pop()
 
+            width = 0
+            if len(d) == 0:
+                width = i
+            
+            else:
+                width = i - d[-1] - 1
+            
+            ans = max(ans, width * data[tmp])
 
+        d.append(i)
+    
+    while len(d) > 0:
+        tmp = d.pop()
 
-
-
-
+        width = 0
+        if len(d) == 0:
+            width = N
+            
+        else:
+            width = N - d[-1] - 1
         
-
-
-
-
-
-'''
-[1000, ]
-'''
         
+        ans = max(ans, width * data[tmp])
 
-
+    print(ans)
