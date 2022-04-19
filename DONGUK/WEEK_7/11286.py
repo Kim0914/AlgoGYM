030ad88e1b68c1e96aea36272a7144546ac20841
kim0914 <kosa0914@naver.com>
2022-04-19 12:34:23 +0900

    04.19

---
+++ DONGUK/WEEK_7/11286.py
@@ -0,0 +1,19 @@
+import heapq, sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+heap = []
+for _ in range(n):
+    c = int(input())
+
+    if c == 0:
+        if len(heap) == 0:
+            print(0)
+        
+        else:
+            print(heapq.heappop(heap)[1])
+    
+    else:
+        heapq.heappush(heap, (abs(c), c))
