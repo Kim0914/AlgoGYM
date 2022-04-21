c0b91e4481742cb6006b9f43e8f4d9b31d78c0c9
kim0914 <kosa0914@naver.com>
2022-04-21 20:45:54 +0900

    04.20

--- DONGUK/WEEK_7/11066.py
+++ DONGUK/WEEK_7/11066.py
@@ -1,26 +1,38 @@
-import sys
+import sys, math
 input = sys.stdin.readline
 
 T = int(input())
 for _ in range(T):
     n = int(input())
-    nums = list(map(int, input().split()))
+    page = list(map(int, input().split()))
 
-    dp = [0] * n
-
+    dp = [[0] * n for _ in range(n)]
+    for j in range(1, n):
+        for i in range(j-1, -1, -1): # 대각선 행렬(자기자신)은 0이기 때문에 계산 X
+            small = math.inf
+            for k in range(j-i):
+                idx = i + k
+                small = min(small, dp[i][i+k] + dp[i+k+1][j])
+            dp[i][j] = small + sum(page[i:j+1])
+    
+    print(dp[0][n-1])
 
 '''
-2
-4
-40 30 30 50
+    A   B   C   D   E
+A   0  A+B
+B       0  B+C
+C           0
+D               0
+E
 
-40 30 = 70
-30 50 = 80
-70 80 = 150
+p에서 q까지 합치는 경우는 다음과 같다.
+p에서 방문할 지점을 r이라고 하면 시작점 p에서 r지점까지 합치는 비용 + 그 다음지점 r+1 부터 마지막 지점 q 까지 합치는 비용이다.
+그 비용들 중에서 가장 작은 비용을 선택한다.
+f(p, q) = (f(p, p) + f(p+1, q)) or (f(p, p+1) + f(p+2, q)) or ....
 
+예를 들어서 f(A, C)는 
+1) A,A -> B,C => A+B + (A+B+C)
+2) A,B -> C,C => B+C + (A+B+C) 
+2개중에 작은 값을 선택하면 구할 수 있다
 
-15
-1 21 3 4 5 35 5 4 3 5 98 21 14 17 32
-
-알바 끝나고 다시 할게여,,,
 '''
--- DONGUK/WEEK_7/1655.py
+++ DONGUK/WEEK_7/1655.py
@@ -3,20 +3,29 @@ input = sys.stdin.readline
 
 n = int(input())
 
-heap = []
+max_heap = []
+min_heap = []
 for i in range(n):
     num = int(input())
-    heapq.heappush(heap, num)
 
     if i == 0:
-        print(heap[0])
+        heapq.heappush(max_heap, (-num, num))
+        print(max_heap[0][1])
         continue
-
-    ind = i // 2
-    if len(heap) % 2 == 1:
-        print(heap[ind])
-        
-    else:
-        print(min(heap[ind], heap[ind + 1]))
     
-    print(heap)
+    if len(max_heap) == len(min_heap):
+        heapq.heappush(max_heap, (-num, num))
+
+    else:
+        heapq.heappush(min_heap, num)
+
+    
+    if max_heap[0][1] > min_heap[0]: 
+        max_top = heapq.heappop(max_heap)[1]
+        min_top = heapq.heappop(min_heap)
+
+        heapq.heappush(max_heap, (-min_top, min_top))
+        heapq.heappush(min_heap, max_top)
+
+    
+    print(max_heap[0][1])
