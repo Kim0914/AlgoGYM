49b892f51b7352cee5902b0909b953d02fbfc66d
kim0914 <kosa0914@naver.com>
2022-04-23 00:22:10 +0900

    04.22

--- DONGUK/WEEK_7/2629.py
+++ DONGUK/WEEK_7/2629.py
@@ -2,7 +2,37 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-data = list(map(int, input().split()))
+nums = list(map(int, input().split()))
+
+candidate = []
+for i in range(n):
+    if len(candidate) == 0:
+        candidate.append(nums[i])
+    
+    else:
+        num = nums[i]
+        
+        for j in range(len(candidate)):
+            diff = abs(num - candidate[j])
+            add = num + candidate[j]
+
+            candidate.append(num)
+            candidate.append(diff)
+            candidate.append(add)
+    
+    candidate = list(set(candidate))
+    
+
 
 t = int(input())
 test = list(map(int, input().split()))
+
+ans = []
+for tc in test:
+    if tc in candidate:
+        ans.append('Y')
+    
+    else:
+        ans.append('N')
+
+print(*ans)
