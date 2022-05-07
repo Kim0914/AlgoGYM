09c4b2a323ebdf51355f954c8e2376c9c24112d4
kim0914 <kosa0914@naver.com>
2022-05-07 13:50:25 +0900

    05.07

---
+++ DONGUK/WEEK_9/9252.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+a = list(input().rstrip())
+b = list(input().rstrip())
+len_a, len_b = len(a), len(b)
+
+dp = [[''] * (len_b + 1) for _ in range(len_a + 1)]
+
+for i in range(1, len_a + 1):
+    for j in range(1, len_b + 1):
+        if a[i-1] == b[j-1]:
+            dp[i][j] = dp[i-1][j-1] + a[i-1]
+            
+        
+        else:
+            if len(dp[i-1][j]) > len(dp[i][j-1]):
+                dp[i][j] = dp[i-1][j]
+
+            else:
+                dp[i][j] = dp[i][j-1]
+
+if len(dp[-1][-1]) == 0:
+    print(0)
+
+else:
+    print(len(dp[-1][-1]))
+    print(dp[-1][-1])
