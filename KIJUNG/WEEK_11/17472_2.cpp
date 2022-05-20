cca22636c680542934464f3b87329a17029cc242
kijung <daiywlf5251@gmail.com>
2022-05-21 00:11:20 +0900

    add comment Problem No: 17472 해결!

--- KIJUNG/WEEK_11/17472_2.cpp
+++ KIJUNG/WEEK_11/17472_2.cpp
@@ -13,7 +13,7 @@ int visited[11][11];
 int comp = 1;
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
-vector<array<int,7> > edge;
+vector<array<int,3> > edge;
 
 void addEdge(int y, int x){
     FOR(i,0,4){
@@ -24,19 +24,13 @@ void addEdge(int y, int x){
             curX += dx[i];
             if(curY<1 || curY>N || curX<1 || curX>M) break;
             int len = max(abs(curY-y),abs(curX-x));
-            // if(len == 1 && visited[curY][curX] == visited[y][x]) break;
             if(map[curY][curX] == 1){
                 if(visited[curY][curX] != visited[y][x]){
                     if(len>2){
-                        // cout << "y: " << y <<", x: " << x << "curY: " << curY <<", curX: " << curX << "\n";
-                        array<int,7> arr;
+                        array<int,3> arr;
                         arr[0] = len-1;
                         arr[1] = visited[y][x];
                         arr[2] = visited[curY][curX];
-                        arr[3] = y;
-                        arr[4] = x;
-                        arr[5] = curY;
-                        arr[6] = curX;
                         edge.push_back(arr);
                         break;
                     }
@@ -81,7 +75,7 @@ int main(void){
         }
     }
 
-    // 컴포넌트 구하기
+    
     FOR(i,1,N+1){
         FOR(j,1,M+1){
             if(visited[i][j] == 0 && map[i][j] == 1){
@@ -90,20 +84,9 @@ int main(void){
             }
         }
     }
-    // cout << "\n";
 
-    // FOR(i,1,N+1){
-    //     FOR(j,1,M+1){
-    //         cout << visited[i][j] << " ";    
-    //     }
-    //     cout << "\n";
-    // }
-    // cout << "\n";
-
-    // 루트 초기화
     FOR(i,1,comp) parent[i] = i;
     
-    // 엣지 구하기
     FOR(i,1,N+1){
         FOR(j,1,M+1){
             if(map[i][j] == 1) addEdge(i,j);
@@ -112,45 +95,15 @@ int main(void){
 
     sort(edge.begin(), edge.end());
 
-    // 가능한 간선 구하기
     FOR(i,0,edge.size()){
-        // cout << edge[i][1] << " " << edge[i][2] << " " << edge[i][0] << "\n";
-        // cout << edge[i][3] << " " << edge[i][4] << " " << edge[i][5] << " " << edge[i][6] << "\n";
         if(findRoot(edge[i][1]) != findRoot(edge[i][2])){
-            // cout << edge[i][1] << " " << edge[i][2] << " " << edge[i][0] << "\n";
-            // cout << edge[i][3] << " " << edge[i][4] << " " << edge[i][5] << " " << edge[i][6] << "\n";
             unionRoot(edge[i][1], edge[i][2]);
             m += edge[i][0];
         }
     }
 
-    // 모든 섬이 연결 가능한가?
     possible();
     cout << m;
 
-
-    // FOR(i,1,N+1){
-    //     FOR(j,1,M+1){
-    //         cout << visited[i][j] << " ";    
-    //     }
-    //     cout << "\n";
-    // }
-
     return 0;
 }
-
-// 8 8
-
-// 0 0 0 1 1 1 1 0
-// 0 1 1 1 1 0 1 0
-// 0 1 0 1 1 1 0 0
-// 0 1 0 0 0 1 0 0
-// 0 0 0 1 0 0 1 0
-// 0 0 0 0 0 1 0 0
-// 0 1 1 1 0 0 0 0
-// 0 1 0 0 0 1 0 0
-
-// 확실히 기출은 다르네
-// 1. dfs로 컴포넌트 구하기
-// 2. 간선구하기(처음에 생각하기 어려웠음. 겹치는 거 고려해야하는 줄 알고..)
-// 3. 크루스칼 돌리기
