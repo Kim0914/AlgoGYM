d6a0d4360ebac4c02005e0b59c828cbe25631bd1
kim0914 <kosa0914@naver.com>
2022-05-22 19:38:32 +0900

    04.21 modify DFS to BFS + Heapq

--- DONGUK/WEEK_7/1520.py
+++ DONGUK/WEEK_7/1520.py
@@ -1,35 +1,39 @@
-import sys
+import sys, heapq
+
 input = sys.stdin.readline
 
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
-def search(x, y):
-    if dp[x][y] != -1:
-        return dp[x][y]
-    
-    if x == m-1 and y == n-1:
-        return 1
-
-    dp[x][y] = 0
-
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-
-        if 0 <= nx < m and 0 <= ny < n:
-            if board[x][y] > board[nx][ny]:
-                dp[x][y] += search(nx, ny)
-
-    return dp[x][y]
-
-
 m, n = map(int ,input().split())
 board = [list(map(int, input().split())) for _ in range(m)]
 
-dp = [[-1] * n for _ in range(m)]
+def search(x, y):
+    q = []
+    heapq.heappush(q, (-board[x][y], x, y))
+
+    visited = [[0] * n for _ in range(m)]
+    visited[x][y] = 1
+
+    while q:
+        now, x, y = heapq.heappop(q)
+        now = now * -1 # heapq에 음수로 들어가 있음
+
+        if x == m-1 and y == n-1:
+            continue
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < m and 0 <= ny < n:
+                if board[nx][ny] < board[x][y]:
+                    if visited[nx][ny] == 0:
+                        heapq.heappush(q, (-board[nx][ny], nx, ny))
+
+                    visited[nx][ny] += visited[x][y]
+                    
+    return visited[-1][-1]
 
 print(search(0,0))
 
-for d in dp:
-    print(d)
