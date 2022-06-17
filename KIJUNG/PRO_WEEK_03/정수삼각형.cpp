a3ea8b879324adddccbc29167776946ecff8c879
kijung <daiywlf5251@gmail.com>
2022-06-17 16:53:53 +0900

    06.17 / 레벨1_내적, 레벨3_정수 삼각형

---
+++ KIJUNG/PRO_WEEK_03/정수삼각형.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> triangle) {
+    for(int i=triangle.size()-2;i>=0;i--){
+        for(int j=0;j<=i;j++){
+            triangle[i][j] += max(triangle[i+1][j],triangle[i+1][j+1]);
+        }
+    }
+    return triangle[0][0];
+}
