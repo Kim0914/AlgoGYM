e004b4f48ffd8a6b5cfd2141c3ad57ac7cc178da
kim0914 <kosa0914@naver.com>
2022-06-19 13:06:22 +0900

    06.19 / 레벨1_소수 만들기

---
+++ DONGUK/프로그래머스_WEEK 3/소수 만들기.py
@@ -0,0 +1,23 @@
+import math
+from itertools import combinations
+
+def isPrime(x):
+    for i in range(2, int(math.sqrt(x)) + 1):
+        if x % i == 0:
+            return False 
+        
+    return True 
+
+
+def solution(nums):
+    answer = 0
+
+    comb_list = list(combinations(nums, 3))
+    
+    ans_list = []
+    
+    for data in comb_list:
+        if isPrime(sum(data)):
+            answer += 1
+            
+    return answer
