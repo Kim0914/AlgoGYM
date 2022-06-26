ec1f0cb0e29bc953d191b84eb3e9b740e1cbabe2
kijung <daiywlf5251@gmail.com>
2022-06-26 23:09:45 +0900

    06.25 / 레벨1_모의고사
    코드를 업데이트 안했네

--- KIJUNG/PRO_WEEK_04/모의고사.cpp
+++ KIJUNG/PRO_WEEK_04/모의고사.cpp
@@ -7,33 +7,22 @@ using namespace std;
 
 vector<int> solution(vector<int> answers) {
     vector<int> answer;
-    int method1[5] = {0,1,2,3,4};
-    int method2[10] = {2,1,2,2,2,3,2,4,2,5};
+    int method1[5] = {1,2,3,4,5};
+    int method2[8] = {2,1,2,3,2,4,2,5};
     int method3[10] = {3,3,1,1,2,2,4,4,5,5};
-    int correct[3] = {0,};
+    int correct[3] = {0,0,0};
 
     for(int i=0;i<answers.size();i++){
-        if(answer[i] == method1[i%5]) correct[0]++;
-        if(answer[i] == method2[i%10]) correct[1]++;
-        if(answer[i] == method3[i%10]) correct[2]++;
+        if(answers[i] == method1[i%5]) correct[0]++;
+        if(answers[i] == method2[i%8]) correct[1]++;
+        if(answers[i] == method3[i%10]) correct[2]++;
     }
 
     int M = *max_element(correct, correct+3);
 
     for(int i=0;i<3;i++){
-        if(correct[i] == M) answer.push_back(i);
+        if(correct[i] == M) answer.push_back(i+1);
     }
     
     return answer;
-}
-
-int main(void){
-    vector<int> answers;
-    answers.push_back(1);
-    answers.push_back(1);
-    answers.push_back(1);
-    answers.push_back(1);
-    answers.push_back(1);
-    solution(answers);
-    return 0;
 }
