0c19f193672662aa04622b2ca64ad13ce5afdcc8
JunHee <80378041+Jun2-Lee@users.noreply.github.com>
2022-06-30 19:18:47 +0900

    6/30

--- JUNHEE/프로그래머스_week5/6_30.예상 대진표.cpp
+++ JUNHEE/프로그래머스_week5/6_30.예상 대진표.cpp
@@ -10,18 +10,9 @@ int solution(int n, int a, int b)
         answer++;
         if(abs(a-b) == 1 && ((a > b && a%2 == 0) || (b > a && b % 2 == 0)))
             break;
-        if(a % 2 == 0){
-            a /= 2;
-        }
-        else{
-            a = (a + 1) / 2;
-        }
-        if(b % 2 == 0){
-            b /= 2;
-        }
-        else{
-            b = (b + 1) / 2;
-        }
+        
+        a = (a + 1) / 2;
+        b = (b + 1) / 2;
     }
 
     return answer;
