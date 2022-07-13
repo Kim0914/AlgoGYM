565fe5351d440acdcc0f611697f7f865f55d7a03
kim0914 <kosa0914@naver.com>
2022-07-13 18:16:48 +0900

    07.13 / 레벨3_등굣길

--- DONGUK/프로그래머스_WEEK 7/등굣길.py
+++ DONGUK/프로그래머스_WEEK 7/등굣길.py
@@ -1,3 +1,50 @@
+from collections import deque
+
+dp = [[0] * (101) for _ in range(101)]
+
+def search(m, n):
+    dx = [0, 1]
+    dy = [1, 0]
+    
+    dp[1][1] = 1
+    
+    q = deque()
+    q.append((1, 1))
+    
+    while q:
+        x, y = q.popleft()
+        
+        for i in range(2):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if 1 <= nx <= n and 1 <= ny <= m:
+                if dp[nx][ny] == -1:
+                    continue
+                else:
+                    dp[nx][ny] += dp[x][y]
+                    
+                    if (nx, ny) not in q:
+                        q.append((nx, ny))
+                        
+    return dp[n][m] % 1000000007
+
+
+
+def solution(m, n, puddles):
+    answer = 0
+    
+    for puddle in puddles:
+        x, y = puddle[0], puddle[1]
+        dp[y][x] = -1
+        
+    answer = search(m, n)
+    
+    return answer
+
+
+
+################## 구글링해서 알게 된 코드 ######################
 def solution(m, n, puddles):
     answer = 0
     
@@ -22,3 +69,5 @@ def solution(m, n, puddles):
     
     answer = dp[n][m] % 1000000007
     return answer
+
+################## 구글링해서 알게 된 코드 ######################
