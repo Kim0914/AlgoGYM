5c6febebbd2a7a1123f736850f07e8fd00576e51
JunHee <80378041+Jun2-Lee@users.noreply.github.com>
2022-07-18 18:52:34 +0900

    7.18 / Level 2 - 2개 이하로 다른 비트

---
+++ JUNHEE/2개 이하로 다른 비트.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<long long> solution(vector<long long> numbers) {
+    vector<long long> answer;
+    for(int i = 0; i < numbers.size(); i++){
+        if(numbers[i] % 2 == 0){
+            answer.push_back(numbers[i] + 1);
+        }
+        else{
+            long long bit = 1;
+            while(true){
+                if((numbers[i] & bit) == 0)
+                    break;
+                bit = bit * 2;
+            }
+            answer.push_back(numbers[i] + (bit / 2));
+        }
+    }
+    return answer;
+}
