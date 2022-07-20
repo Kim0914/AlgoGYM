ddd4a52dae2f0946aee3fbeb4dbfee01ea018f37
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2022-07-20 19:52:14 +0900

    7.20 / Level 2 - 영어 끝말잇기

---
+++ JINSEOP/프로그래머스 WEEK_8/영어 끝말잇기.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+unordered_set<string> validator;
+pair<int, int> ans;
+void word_relay(int n, vector<string> words) {
+    int person_cnt = 2, cnt = 1;
+    validator.insert(words[0]);
+
+    for (int i = 1; i < words.size(); i++) {
+        if (i % n == 0) cnt++;
+        int v_size = validator.size();
+        validator.insert(words[i]);
+
+        if (words[i - 1][words[i - 1].size() - 1] != words[i][0]) {
+            ans.first = person_cnt;
+            ans.second = cnt;
+            return;
+        }
+        if (v_size == validator.size()) {
+            ans.first = person_cnt;
+            ans.second = cnt;
+            return;
+        }
+
+        person_cnt++;
+        if (person_cnt > n) person_cnt = 1;
+    }
+
+    person_cnt = 0; cnt = 0;
+}
+
+vector<int> solution(int n, vector<string> words) {
+    vector<int> answer;
+
+    word_relay(n, words);
+    answer.push_back(ans.first);
+    answer.push_back(ans.second);
+
+    return answer;
+}
