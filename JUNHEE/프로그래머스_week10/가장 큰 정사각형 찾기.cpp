858b091683a930aa4874c4037a77a41e05ad530b
JunHee <80378041+Jun2-Lee@users.noreply.github.com>
2022-08-05 19:33:15 +0900

    8.5 / Level 2 - 가장 큰 정사각형 찾기

---
+++ JUNHEE/프로그래머스_week10/가장 큰 정사각형 찾기.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<vector<int>> board)
+{
+    int answer = board[0][0];
+
+    for(int i = 1; i < board.size(); i++){
+        for(int j = 1; j < board[i].size(); j++){
+            if(board[i][j] == 1){
+                board[i][j] = min({board[i-1][j], board[i-1][j-1], board[i][j-1]}) + 1;
+                if(answer < board[i][j])
+                    answer = board[i][j];
+            }
+            
+        }
+    }
+    answer *= answer;
+
+    return answer;
+}
