8e958143cd693636b0be233c1349b39bf3a7e3f1
JunHee <ljh19980823@gmail.com>
2022-09-06 21:07:50 +0900

    9.7 / Level 2 - 다음 큰 숫자

---
+++ JUNHEE/프로그래머스_week11/다음 큰 숫자.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    bool flag = true;
+    int next = n + 1;
+    int count = 0;
+    while(n){
+        if(n % 2 == 1) count++;
+        n /= 2;
+    }
+    while(flag){
+        int t_next = next, t_count = 0;
+        while(t_next){
+            if(t_next % 2 == 1) t_count++;
+            t_next /= 2;
+        }
+        if(t_count == count) flag = false;
+        else next++;
+    }
+    cout << next;
+    return next;
+}
