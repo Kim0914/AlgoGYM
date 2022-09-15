204597ae960df32a8b79f5ccb85630287f64dfb2
JunHee <ljh19980823@gmail.com>
2022-09-15 15:34:38 +0900

    9.14 / Level 2 - 행렬의 곱셈

---
+++ JUNHEE/프로그래머스_week12/행렬의 곱셈.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+    vector<vector<int>> answer;
+    for(int i = 0; i < arr1.size(); i++){
+        vector<int> temp;
+        for(int j = 0; j < arr2[0].size(); j++){
+            int tmp = 0;
+            for(int k = 0; k < arr1[0].size(); k++){
+                tmp += arr1[i][k] * arr2[k][j];
+            }
+            temp.push_back(tmp);
+        }
+        answer.push_back(temp);
+    }
+    return answer;
+}
