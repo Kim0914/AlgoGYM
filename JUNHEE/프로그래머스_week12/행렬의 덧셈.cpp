a9a5262586a373f29cb4bcc3459977b9488f2e32
JunHee <ljh19980823@gmail.com>
2022-09-16 18:04:37 +0900

    9.15 / Level 1 - 행렬의 덧셈

--- JUNHEE/프로그래머스_week12/행렬의 덧셈.cpp
+++ JUNHEE/프로그래머스_week12/행렬의 덧셈.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
     vector<vector<int>> answer;
+    
     for(int i = 0; i < arr1.size(); i++){
         vector<int> temp;
         for(int j = 0; j < arr1[0].size(); j++){
