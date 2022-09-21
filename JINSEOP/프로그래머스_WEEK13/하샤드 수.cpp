b11c0f157c14cf02b38e2dc222c9305b1a8d07e0
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2022-09-21 22:59:52 +0900

    아직 미완성

---
+++ JINSEOP/프로그래머스_WEEK13/하샤드 수.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void calculate_partition(int x) {
+    string temp = "";
+
+}
+
+bool solution(int x) {
+    bool answer = true;
+
+    
+    return answer;
+}
+
+int main() {
+    cout << solution(10);
+	return 0;
+}
