4bde4e4f91e2f71a80d91b4d99f14b466be0f124
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2022-09-21 23:18:38 +0900

    9.21 / LEVEL 1 - 하샤드 수

--- JINSEOP/프로그래머스_WEEK13/하샤드 수.cpp
+++ JINSEOP/프로그래머스_WEEK13/하샤드 수.cpp
@@ -1,21 +1,28 @@
-#include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
 
-void calculate_partition(int x) {
-    string temp = "";
+int calculate_partition(int x) {
+    int sum = 0;
 
+    while (x != 0) {
+        sum = sum + (x - ((x / 10) * 10));
+        x /= 10;
+    }
+
+    return sum;
+}
+
+bool check_harshard(int x, int sum) {
+    if (x % sum == 0) return true;
+    else return false;
 }
 
 bool solution(int x) {
     bool answer = true;
-
     
-    return answer;
-}
+    int check_sum = calculate_partition(x);
+    answer = check_harshard(x, check_sum);
 
-int main() {
-    cout << solution(10);
-	return 0;
+    return answer;
 }
