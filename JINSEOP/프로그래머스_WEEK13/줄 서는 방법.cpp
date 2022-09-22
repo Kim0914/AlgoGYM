04287aaecfacaa2297489b5b07add843800283ca
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2022-09-22 14:41:27 +0900

    Update 줄 서는 방법.cpp

--- JINSEOP/프로그래머스_WEEK13/줄 서는 방법.cpp
+++ JINSEOP/프로그래머스_WEEK13/줄 서는 방법.cpp
@@ -18,12 +18,13 @@ void make_vector(int n) {
 void calculate_permutation(int n, long long k, vector<int>& answer) {
     long long factor = DP[n];
     long long modular = 0, divisor = 0;
-    k--;
-
+    k--; // 왜 1을 빼는가?
+         // 0, 1 / 2, 3/ 4, 5 와 같이 나눴을 때 묶기 위해서.
+    
     while (permutation.size() > 1) {
-        factor /= n;
-        modular = k % factor;
-        divisor = k / factor;
+        factor /= n; // (n! / n) 하면 몇 칸으로 나눠지는지 나옴. 우리는 계속 이 값을 이용할 것이다.
+        modular = k % factor; // 4! => 3! => 2! => 1! 으로 넘어갈 때 마다 k값이 바뀌어야 하는데, 그 Position을 정해준다.
+        divisor = k / factor; // k가 몇 번째 칸에 위치하는지, 그리고 해당 값을 빼낼 idx의 역할을 한다. 
 
         answer.push_back(permutation[divisor]);
         permutation.erase(permutation.begin() + divisor);
