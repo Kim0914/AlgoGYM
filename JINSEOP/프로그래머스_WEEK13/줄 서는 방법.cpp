b5d64fd741b9ce05e575a8f305f17176317dcd2a
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2022-09-22 14:38:10 +0900

    9.22 / LEVEL 2 - 줄 서는 방법

--- JINSEOP/프로그래머스_WEEK13/줄 서는 방법.cpp
+++ JINSEOP/프로그래머스_WEEK13/줄 서는 방법.cpp
@@ -2,45 +2,45 @@
 #include <vector>
 using namespace std;
 
-string permutation = "";
-long long cnt = 0;
-bool is_end = false;
-void backtrack(long long k, vector<int>& visit) {
-    if (permutation.size() == visit.size()) {
-        cnt++;
-        if (cnt == k)
-            is_end = true;
-        return;
-    }
-
-    for (int i = 0; i < visit.size(); i++) {
-        if (!visit[i]) {
-            visit[i] = true;
-            permutation += ((i + 1) + '0');
-            backtrack(k, visit);
-            if (is_end) return;
-            visit[i] = false;
-            permutation.pop_back();
-        }
-    }
+long long DP[21];
+vector<int> permutation;
+void init_factorial(int n) {
+    DP[0] = 1;
+    for (int i = 1; i <= n; i++)
+        DP[i] = i * DP[i - 1];
 }
 
-void string_vectorize(vector<int>& answer) {
-    for (char c : permutation)
-        answer.push_back(c - '0');
+void make_vector(int n) {
+    for (int i = 1; i <= n; i++)
+        permutation.push_back(i);
+}
+
+void calculate_permutation(int n, long long k, vector<int>& answer) {
+    long long factor = DP[n];
+    long long modular = 0, divisor = 0;
+    k--;
+
+    while (permutation.size() > 1) {
+        factor /= n;
+        modular = k % factor;
+        divisor = k / factor;
+
+        answer.push_back(permutation[divisor]);
+        permutation.erase(permutation.begin() + divisor);
+        
+        k = modular;
+        n--;
+    }
+
+    answer.push_back(permutation[0]);
 }
 
 vector<int> solution(int n, long long k) {
     vector<int> answer;
-    vector<int> visit(n, 0);
-    
-    backtrack(k, visit);
-    string_vectorize(answer);
+
+    make_vector(n);
+    init_factorial(n);
+    calculate_permutation(n, k, answer);
 
     return answer;
-}
-
-int main() {
-    cout << solution(3, 5);
-	return 0;
 }
