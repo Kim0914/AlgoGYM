e63d3bcbcc45d8aae0ff7b5b783bf2e367cb45c2
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2022-09-27 23:12:40 +0900

    9.27 / LEVEL 1 - 짝수와 홀수

---
+++ JINSEOP/프로그래머스_WEEK13/짝수와 홀수.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num) {
+    string answer = "";
+    num % 2 == 0 ? answer = "Even" : answer = "Odd"; 
+    
+    return answer;
+}
