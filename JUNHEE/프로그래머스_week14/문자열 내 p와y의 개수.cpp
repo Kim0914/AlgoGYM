6d4b08c123971a479622498abb67e26ab8ce9ec3
JunHee <ljh19980823@gmail.com>
2022-09-30 09:20:52 +0900

    9.29 / Level 1 - 문자열 내 p와y의 개수

---
+++ JUNHEE/프로그래머스_week14/문자열 내 p와y의 개수.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    int p = 0;
+    int y = 0;
+    for(int i = 0; i < s.size(); i++){
+        if(s[i] == 'p' || s[i] == 'P')
+            p++;
+        else if(s[i] == 'y' || s[i] == 'Y')
+            y++;
+    }
+    if(p == y) return true;
+    else return false;
+}
