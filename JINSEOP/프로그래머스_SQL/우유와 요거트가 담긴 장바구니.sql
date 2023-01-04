de9f079380236a8b902e5b87c1ca4f6a45de9b7e
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2023-01-05 00:13:57 +0900

    LEVEL 4 - 우유와 요거트가 담긴 장바구니

--- JINSEOP/프로그래머스_SQL/우유와 요거트가 담긴 장바구니.sql
+++ JINSEOP/프로그래머스_SQL/우유와 요거트가 담긴 장바구니.sql
@@ -4,3 +4,5 @@ WHERE NAME = 'Milk' AND CART_ID IN(SELECT CART_ID
                                 FROM CART_PRODUCTS
                                 WHERE NAME = 'Yogurt')
 ORDER BY CART_ID
+
+// IN ANY ALL에 대한 이해가 필요하다.
