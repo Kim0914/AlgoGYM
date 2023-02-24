24f20eecdc8c5346b9a9a60ed05e1fe4f98f34d2
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2023-02-24 23:21:22 +0900

    LEVEL 3 - 숫자 게임

--- JINSEOP/프로그래머스_혼자_풀기/숫자 게임.cpp
+++ JINSEOP/프로그래머스_혼자_풀기/숫자 게임.cpp
@@ -1,21 +1,33 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <queue>
 using namespace std;
+
+priority_queue<int, vector<int>, greater<>> A_q;
+priority_queue<int, vector<int>, greater<>> B_q;
+void fill_queue(vector<int> A, vector<int> B) {
+    for (int i = 0; i < A.size(); i++) {
+        A_q.push(A[i]);
+        B_q.push(B[i]);
+    }
+}
 
 int solution(vector<int> A, vector<int> B) {
     int answer = 0;
-    sort(B.begin(), B.end());
 
-    for (int i = 0; i < A.size(); i++) {
-        for (int j = 0; j < B.size(); j++) {
-            if (A[i] < B[j]) {
-                B.erase(B.begin() + j);
-                answer++;
-                break;
-            }
+    fill_queue(A, B);
+    while (!B_q.empty()) {
+        if (A_q.top() < B_q.top()) {
+            A_q.pop();
+            B_q.pop();
+            answer++;
+            continue;
         }
+
+        B_q.pop();
     }
 
     return answer;
 }
+
+// 정렬을 해도 상관이 없다는 것이 핵심!
