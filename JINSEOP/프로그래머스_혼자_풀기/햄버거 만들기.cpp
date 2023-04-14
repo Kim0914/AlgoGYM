eb021c6b52a789112d1e9cc72c6ffda0fe13cb50
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2023-04-14 22:43:05 +0900

    아직 진행 중 왜 안되냐

--- JINSEOP/프로그래머스_혼자_풀기/햄버거 만들기.cpp
+++ JINSEOP/프로그래머스_혼자_풀기/햄버거 만들기.cpp
@@ -1,15 +1,39 @@
 #include <string>
 #include <vector>
 #include <stack>
+#include <iostream>
 using namespace std;
 
 stack<int> hamburger;
+vector<int> manual = {1, 2, 3, 1};
 int solution(vector<int> ingredient) {
-    int answer = 0;
+    int answer = 0, iter = 0, man_ptr = 0;
     // 기본 순서는 1 2 3 1
 
-    for (int i = 0; i < ingredient.size(); i++) {
-        hamburger.push(ingredient[i]);
+    while(true) {
+        if (man_ptr == 1 && hamburger.top() == 1)
+            man_ptr = 0;
+
+        if (!hamburger.empty() && (hamburger.top() == manual[man_ptr])) {
+            man_ptr++;
+
+            if (man_ptr == manual.size()) {
+                for (int i = 0; i < 4; i++)
+                    hamburger.pop();
+
+                man_ptr = 0;
+                answer++;
+                continue;
+            }
+        }
+        else
+            man_ptr = 0;
+
+        if (iter >= ingredient.size())
+            break;
+
+        hamburger.push(ingredient[iter]);
+        iter++;
     }
 
     return answer;
@@ -17,6 +41,6 @@ int solution(vector<int> ingredient) {
 
 int main() {
     vector<int> ingredient = { 2, 1, 1, 2, 3, 1, 2, 3, 1 };
-    solution(ingredient);
+    cout << solution(ingredient);
     return 0;
 }
