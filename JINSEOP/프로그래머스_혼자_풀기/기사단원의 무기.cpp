114c2f73fe828467346b8587b7398d982db3aa28
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2023-04-16 12:44:52 +0900

    LEVEL 2 - 기사단원의 무기

---
+++ JINSEOP/프로그래머스_혼자_풀기/기사단원의 무기.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+void get_divisors(vector<int> &divisors) {
+    for (int i = 1; i < divisors.size(); i++) {
+        for (int j = i; j < divisors.size(); j += i)
+            divisors[j]++;
+    } // 억억단 그대로 적용
+}
+
+void calculate_weight(int number, int limit, int power, int &answer, vector<int> divisors) {
+    for (int i = 1; i <= number; i++) {
+        if (divisors[i] > limit)
+            answer += power;
+        else
+            answer += divisors[i];
+    }
+}
+
+int solution(int number, int limit, int power) {
+    int answer = 0;
+    vector<int> divisors(number + 1, 0);
+
+    get_divisors(divisors);
+    calculate_weight(number, limit, power, answer, divisors);
+
+    return answer;
+}
