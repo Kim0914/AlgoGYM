1a44465171bb0ab26e87142627240f0657a21ea3
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2023-04-18 23:52:06 +0900

    도전 중

---
+++ JINSEOP/프로그래머스_혼자_풀기/옹알이(2).cpp
@@ -0,0 +1,53 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+using namespace std;
+
+unordered_map<string, pair<int, string>> babbling_map;
+void fill_map() {
+    babbling_map["aya"] = {3, "1"};
+    babbling_map["ye"] = {2, "2"};
+    babbling_map["woo"] = {3, "3"};
+    babbling_map["ma"] = {2, "4"};
+}
+
+int solution(vector<string> babbling) {
+    int answer = 0;
+
+    fill_map();
+
+    for (string s : babbling) {
+        int len = 0;
+        string partial_bab = "", validator = "";
+
+        for (int i = 0; i < s.size(); i++) {
+            partial_bab += s[i];
+
+            if (babbling_map[partial_bab].first != 0) {
+                len += babbling_map[partial_bab].first;
+                partial_bab = "";
+                validator += babbling_map[partial_bab].second;
+
+                cout << validator << endl;
+                if (validator.size() > 1) {
+                    if (validator[validator.size() - 2] == validator[validator.size() - 1]) {
+                        len = 0;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (len == s.size())
+            answer++;
+    }
+
+    return answer;
+}
+
+int main() {
+    vector<string> babbling = { "ayaye", "uuu", "yeye", "yemawoo", "ayaayaa" };
+    cout << solution(babbling);
+    return 0;
+}
