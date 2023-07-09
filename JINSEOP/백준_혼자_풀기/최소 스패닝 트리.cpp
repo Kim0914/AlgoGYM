a6afe71958649e619d74e5007e10012faf090bf6
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2023-07-09 13:08:52 +0900

    MST 기본, Kruskal

--- JINSEOP/백준_혼자_풀기/최소 스패닝 트리.cpp
+++ JINSEOP/백준_혼자_풀기/최소 스패닝 트리.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 int parent[10001]; // 그래프
 long long answer = 0;
-bool noCycle = false;
 vector<pair<int, pair<int, int>>> graph;
 void init_map(int size) {
 	for (int i = 1; i <= size; i++)
@@ -17,22 +16,24 @@ int get_parent(int node) {
 	return parent[node] = get_parent(parent[node]);
 }
 
-void do_union(int node_x, int node_y) {
-	noCycle = false;
-
+bool checkCycle(int node_x, int node_y) {
+	// Cycle이 존재하는 지 확인하는 함수
 	node_x = get_parent(node_x);
 	node_y = get_parent(node_y);
 
-	// 부모가 같은데 연결을 한다?
-	// 사이클이 생긴다는 것이므로 연결하지 않는다.
+	// 부모가 같으면 연결했을 때, Cycle이 발생한다.
 	if (node_x == node_y)
-		return;
-	else {
-		if (node_x < node_y) parent[node_x] = node_y;
-		else parent[node_y] = node_x;
+		return true;
 
-		noCycle = true;
-	}
+	return false;
+}
+
+void do_union(int node_x, int node_y) {
+	node_x = get_parent(node_x);
+	node_y = get_parent(node_y);
+
+	if (node_x < node_y) parent[node_x] = node_y;
+	else parent[node_y] = node_x;
 }
 
 int main() {
@@ -50,8 +51,11 @@ int main() {
 
 	// 이제부터 간선을 잇는다.
 	for (int i = 0; i < ed; i++) {
-		do_union(graph[i].second.first, graph[i].second.second);
-		if (noCycle) answer += graph[i].first;
+		if (!checkCycle(graph[i].second.first, graph[i].second.second)) {
+			// Cycle 발생 가능성이 없으면 아래의 로직 진행
+			do_union(graph[i].second.first, graph[i].second.second);
+			answer += graph[i].first;
+		}
 	}
 
 	cout << answer;
