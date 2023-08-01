cb660fcaf0b8876bee3e84edc3ba4ab3b2b3c8d7
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2023-08-01 22:40:42 +0900

    파라메트릭 서치

--- JINSEOP/백준_혼자_풀기/이분 탐색/기타 레슨.cpp
+++ JINSEOP/백준_혼자_풀기/이분 탐색/기타 레슨.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 #define ll long long
 
-ll num = 0, limit = 0, lesson = 0, sum = 0, answer = 9999999999;
+ll num = 0, limit = 0, lesson = 0, sum = 0;
 vector<int> lessons;
 
 ll checksum(ll mid) {
@@ -24,7 +24,7 @@ ll checksum(ll mid) {
 	return cnt;
 }
 
-void binary_search(ll left, ll right) {
+void binary_search(ll left, ll right, ll &result) {
 	if (left > right)
 		return;
 
@@ -32,23 +32,26 @@ void binary_search(ll left, ll right) {
 	ll lesson_cnt = checksum(mid);
 
 	if (lesson_cnt <= limit)
-		binary_search(left, mid - 1);
+		binary_search(left, mid - 1, result);
 	else {
-		answer = mid + 1;
-		binary_search(mid + 1, right);
+		result = mid + 1;
+		binary_search(mid + 1, right, result);
 	}
 }
 
 int main() {
+	ll init_left = -1;
 	cin >> num >> limit;
 	for (int i = 0; i < num; i++) {
 		cin >> lesson;
 		sum += lesson;
+		init_left = max(init_left, lesson); // 핵심은 여기! 왼쪽 끝이 가장 큰 레슨이어야 한다.
 		lessons.push_back(lesson);
 	}
 
-	binary_search(1, sum); // 최소 1분, 최대 합분까지
-	cout << answer;
+	ll result = init_left; // 정답을 왼쪽 끝으로 초기화
+	binary_search(init_left, sum, result); // 최소 1분, 최대 합분까지
+	cout << result;
 
 	return 0;
 }
