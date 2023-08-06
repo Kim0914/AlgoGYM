753c5384257a8d653f64ead617169242d0b3773f
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2023-08-07 01:29:07 +0900

    음.. 그리디

--- JINSEOP/백준_혼자_풀기/그리디/서강근육맨.cpp
+++ JINSEOP/백준_혼자_풀기/그리디/서강근육맨.cpp
@@ -1,13 +1,35 @@
 #include <iostream>
-#include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
+#define ll long long
 
-int num = 0, loss = 0, workout[];
+ll num = 0, loss = 0, min_loss = 0;
+vector<ll> work_out;
 int main() {
 	cin >> num;
 	for (int i = 0; i < num; i++) {
 		cin >> loss;
+		work_out.push_back(loss);
 	}
+
+	sort(work_out.begin(), work_out.end());
+
+	if (num % 2 != 0) {
+		min_loss = work_out[num - 1];
+		num -= 1; // 제일 큰 한 놈을 제외하고 운동한다.
+
+		for (int i = 0; i < num / 2; i++)
+			min_loss = max(min_loss, (work_out[i] + work_out[(num - 1) - i]));
+
+		min_loss = max(min_loss, work_out[work_out.size() - 1]);
+	}
+	else {
+		for (int i = 0; i < num / 2; i++)
+			min_loss = max(min_loss, (work_out[i] + work_out[(num - 1) - i]));
+	}
+
+	cout << min_loss;
 
 	return 0;
 }
