f874d8a1a89091c4672f32e834423f652f6b5893
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2023-08-06 23:45:57 +0900

    Create 서강근육맨.cpp

---
+++ JINSEOP/백준_혼자_풀기/그리디/서강근육맨.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int num = 0, loss = 0, workout[];
+int main() {
+	cin >> num;
+	for (int i = 0; i < num; i++) {
+		cin >> loss;
+	}
+
+	return 0;
+}
