54231842da6e8715e9cc6acca335c35c2ced2847
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2023-09-01 15:52:57 +0900

    BFS + 다익스트라 녹색 옷이 젤다지!

--- JINSEOP/백준_혼자_풀기/그래프 탐색/녹색 옷 입은 애가 젤다지?.cpp
+++ JINSEOP/백준_혼자_풀기/그래프 탐색/녹색 옷 입은 애가 젤다지?.cpp
@@ -4,36 +4,42 @@
 using namespace std;
 #define INF 99999999
 
-int row = 0, answer = INF, cnt = 0, cave_map[125][125];
+int row = 0, answer = INF, cnt = 0, cave_map[125][125], dist[125][125];
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = { 0, 1, 0, -1 };
-bool visit[125][125];
-void bfs(pair<int, int> start) {
-	queue<pair <int, pair<int, int>>> bfs_q;
-	bfs_q.push({ cave_map[0][0], start });
-	visit[0][0] = true;
+void fill_dist() {
+	for (int i = 0; i < row; i++)
+		for (int j = 0; j < row; j++)
+			dist[i][j] = INF;
+}
 
-	while (!bfs_q.empty()) {
-		pair<int, int> curr = bfs_q.front().second;
-		int curr_cost = bfs_q.front().first;
-		bfs_q.pop();
+void dijkstra(pair<int, int> start) {
+	priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> dijk_q;
+	dijk_q.push({ cave_map[0][0], start });
+	dist[0][0] = cave_map[0][0];
+	// 다익스트라 시작점 초기화
 
-		cout << curr.first << " " << curr.second << '\n';
-		if (curr.first == row - 1 && curr.second == row - 1)
-			answer = min(answer, curr_cost);
+	while (!dijk_q.empty()) {
+		pair<int, int> curr = dijk_q.top().second;
+		int curr_cost = dijk_q.top().first;
+		dijk_q.pop();
 
 		for (int i = 0; i < 4; i++) {
 			int nx = curr.first + dx[i];
-			int ny = curr.second + dy[i];
+			int ny = curr.second + dy[i]; // BFS 동일하게 진행
 
 			if ((nx >= 0 && nx < row) && (ny >= 0 && ny < row)) {
-				if (!visit[nx][ny]) {
-					bfs_q.push({ curr_cost + cave_map[nx][ny], {nx, ny} });
-					visit[nx][ny] = true;
+				int next_cost = curr_cost + cave_map[nx][ny];
+
+				if (dist[nx][ny] > next_cost) {
+					dist[nx][ny] = next_cost; // 새로운 값이 더 작으면 갱신
+					dijk_q.push({dist[nx][ny], {nx, ny}});
 				}
 			}
 		}
 	}
+	
+	answer = dist[row - 1][row - 1];
 }
 
 int main() {
@@ -48,8 +54,9 @@ int main() {
 		for (int i = 0; i < row; i++)
 			for (int j = 0; j < row; j++)
 				cin >> cave_map[i][j];
-
-		bfs({0, 0});
+		
+		fill_dist();
+		dijkstra({0, 0});
 		cout << "Problem " << cnt << ": " << answer << '\n';
 	}
 
