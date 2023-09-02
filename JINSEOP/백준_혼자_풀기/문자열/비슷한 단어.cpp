11628be6310f66c370340b919b0ed0cd22f6dec0
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2023-09-03 01:56:50 +0900

    왜 안돼유~

--- JINSEOP/백준_혼자_풀기/문자열/비슷한 단어.cpp
+++ JINSEOP/백준_혼자_풀기/문자열/비슷한 단어.cpp
@@ -1,1 +1,39 @@
-ㅇㅇ
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+int num = 0, max_cnt = 0, max_first_idx = 0, max_second_idx = 0;
+string word = "";
+unordered_map<string, pair<int, int>> word_map;
+vector<string> word_list;
+int main() {
+	cin >> num;
+	for (int i = 0; i < num; i++) {
+		cin >> word; 
+		word_list.push_back(word);
+		string temp = "";
+		int overlap_cnt = 0;
+
+		for (int j = 0; j < word.size(); j++) {
+			temp += word[j];
+
+			if (word_map[temp].first > 0) {
+				overlap_cnt = word_map[temp].first;
+
+				if (overlap_cnt > max_cnt) {
+					max_cnt = overlap_cnt;
+					max_first_idx = word_map[temp].second;
+					max_second_idx = i;
+				}
+			}
+
+			word_map[temp] = { temp.size() , i };
+		}
+	}
+
+	cout << word_list[max_first_idx] << '\n';
+	cout << word_list[max_second_idx];
+
+	return 0;
+}
