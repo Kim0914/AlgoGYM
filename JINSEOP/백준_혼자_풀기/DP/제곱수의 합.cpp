c795891e1b7ae81dc95c1d616bbaa39186bb539a
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2023-09-06 21:24:19 +0900

    DP 어려바 어려둬

--- JINSEOP/백준_혼자_풀기/DP/제곱수의 합.cpp
+++ JINSEOP/백준_혼자_풀기/DP/제곱수의 합.cpp
@@ -9,9 +9,12 @@ void init_dp() {
 }
 
 void calc_square() {
-	for (int i = 1; i <= num; i++)
+	for (int i = 1; i <= num; i++) // 숫자를 채워간다.
 		for (int j = 1; j * j <= i; j++)
+			// 제곱수가 i를 넘지 않는 선에서
 			dp[i] = min(dp[i], dp[i - j * j] + 1);
+	// 현재 목표 수 - 제곱수를 한 것이 더 작을 경우 치환
+	// +1을 하는 이유는? 해당 제곱수를 써야하기 때문
 }
 
 int main() {
