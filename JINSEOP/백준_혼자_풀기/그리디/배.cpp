b8e49220e2e3a14959b796fabad77a751dfca177
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2023-09-09 17:47:00 +0900

    Erase하는 시간이 훨씬 빠르다

--- JINSEOP/백준_혼자_풀기/그리디/배.cpp
+++ JINSEOP/백준_혼자_풀기/그리디/배.cpp
@@ -6,15 +6,6 @@ using namespace std;
 int num = 0, answer = 0, crane = 0, contain = 0;
 vector<int> cranes, container;
 bool pos[10000];
-bool check_container() {
-	for (int i = 0; i < container.size(); i++) {
-		if (container[i] != -1)
-			return true;
-	}
-
-	return false;
-}
-
 int main() {
 	cin >> num;
 	for (int i = 0; i < num; i++) {
@@ -33,19 +24,16 @@ int main() {
 	if (cranes[0] < container[0])
 		answer = -1;
 	else {
-		while (true) {
+		while (!container.empty()) {
 			answer++;
 			for (int i = 0; i < cranes.size(); i++) {
 				for (int j = 0; j < container.size(); j++) {
-					if (container[j] != -1 && (container[j] <= cranes[i])) {
-						container[j] = -1; // 컨테이너에서 큰 박스부터 순서대로 처리
+					if (container[j] <= cranes[i]) {
+						container.erase(container.begin() + j); // 컨테이너에서 큰 박스부터 순서대로 처리
 						break;
 					}
 				}
 			}
-
-			if (!check_container())
-				break;
 		}
 	}
 
