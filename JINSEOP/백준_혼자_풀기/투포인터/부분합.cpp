20573480308d17fd6a9839e724629559124780ed
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2023-09-13 00:28:27 +0900

    투포인터

--- JINSEOP/백준_혼자_풀기/투포인터/부분합.cpp
+++ JINSEOP/백준_혼자_풀기/투포인터/부분합.cpp
@@ -1,53 +1,41 @@
 #include <iostream>
-#include <vector>
 using namespace std;
+#define MAX 99999999;
 
-vector<int> sequence;
-int two_pointer(int sum_bound) {
-    int front_ptr = 0, rear_ptr = 0, sum = 0, len = 0, min_len = 999999;
+int num = 0, seq[100000], answer = MAX;
+long long sum = 0;
+void two_pointer() {
+	int rear_ptr = 0, front_ptr = 0;
+	long long checksum = 0;
+	
+	while (rear_ptr < num) {
+		if (front_ptr < num) {
+			checksum += seq[front_ptr];
+			front_ptr++;
+		}
 
-    while (rear_ptr < sequence.size()) {
-        if (front_ptr < sequence.size()) {
-            sum += sequence[front_ptr];
-            front_ptr++;
-            len++;
-        }
+		if (front_ptr == num && checksum < sum)
+			break;
 
-        if (front_ptr == sequence.size() && (sum < sum_bound))
-            break;
-
-        if(sum >= sum_bound) {
-            if (min_len > len)
-                min_len = len;
-
-            while (sum >= sum_bound) {
-                if (min_len > len)
-                    min_len = len;
-
-                sum -= sequence[rear_ptr];
-                rear_ptr++;
-                len--;
-            }
-        }
-    }
-
-    return min_len;
+		while (checksum >= sum) {
+			answer = min(answer, front_ptr - rear_ptr);
+			checksum -= seq[rear_ptr];
+			rear_ptr++;
+		}
+	}
 }
 
 int main() {
-    int seq = 0, num = 0, sum_bound = 0, result_length = 0;
+	cin >> num >> sum;
+	for (int i = 0; i < num; i++) {
+		cin >> seq[i];
+	}
 
-    cin >> seq >> sum_bound;
-    for (int i = 0; i < seq; i++) {
-        cin >> num;
-        sequence.push_back(num);
-    }
+	two_pointer();
+	if (answer == 99999999)
+		cout << 0;
+	else
+		cout << answer;
 
-    result_length = two_pointer(sum_bound);
-
-    if (result_length == 999999)
-        result_length = 0;
-    cout << result_length;
-
-    return 0;
+	return 0;
 }
