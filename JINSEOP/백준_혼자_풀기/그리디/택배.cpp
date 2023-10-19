cec24a8a8232b9be4587827a5c63b13a1aba9516
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2023-10-19 16:22:53 +0900

    진짜 생각해내기 어려운 그리디

---
+++ JINSEOP/백준_혼자_풀기/그리디/택배.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool custom_cmp(pair<int, pair<int, int>> a, pair<int, pair<int, int>> b) {
+	if (a.second.first == b.second.first)
+		return a.first < b.first;
+	// tie break 시 출발지 기준 내림차순
+	return a.second.first < b.second.first;
+	// 도착지 기준 내림차순 정렬
+}
+
+vector<pair<int, pair<int, int>>> parcel_info;
+int num = 0, limit = 0, info_num = 0, src = 0, dest = 0, weight = 0, answer = 0;
+int loads[2001];
+int main(){
+	cin >> num >> limit;
+	cin >> info_num;
+	for (int i = 0; i < info_num; i++) {
+		cin >> src >> dest >> weight;
+		parcel_info.push_back({ src, {dest, weight} });
+	}
+
+	sort(parcel_info.begin(), parcel_info.end(), custom_cmp);
+
+	for (int i = 0; i < parcel_info.size(); i++) {
+		src = parcel_info[i].first;
+		dest = parcel_info[i].second.first;
+		int weight = parcel_info[i].second.second;
+		int max_loads = 0; // 택배가 마을을 지날 때 싣고있는 최대 무게
+
+		for (int j = src; j < dest; j++)
+			max_loads = max(max_loads, loads[j]);
+
+		int capacity = min(weight, limit - max_loads);
+		// 지금 가져가야 할 택배를 모두 실을 수 있는가?
+		// 아니면 싣고있는 최대 무게 만큼 빼고 들고가야 하는가
+		for (int j = src; j < dest; j++)
+			loads[j] += capacity;
+		// 해당 마을을 향할 때, 해당 용량만큼 택배를 실을 수 있다는 의미
+		answer += capacity;
+		// 성공적으로 트럭이 실어진 택배
+	}
+
+	cout << answer;
+	return 0;
+}
