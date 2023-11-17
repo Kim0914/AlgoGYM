9027714f409f08cd6917f85395069e93d43791c2
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2023-11-17 23:45:54 +0900

    DP

--- JINSEOP/백준_혼자_풀기/DP/가장 긴 증가하는 부분 수열 4.cpp
+++ JINSEOP/백준_혼자_풀기/DP/가장 긴 증가하는 부분 수열 4.cpp
@@ -35,14 +35,15 @@ int main() {
 
     for (int i = num; i >= 0; i--) {
         if (dp[i] == answer) {
+            // 어차피 가장 마지막에 갱신되는 수가 정답 수열이다.
+            // 뒤에서부터 훑어 해당 길이에 맞는 수를 저장하자!
             answer_vec.push_back(seq[i]);
             answer--;
         }
     }
 
-    for (int i = answer_vec.size()-1; i >= 0; i--) {
+    for (int i = answer_vec.size()-1; i >= 0; i--)
         cout << answer_vec[i] << " ";
-    }
 
     return 0;
 }
