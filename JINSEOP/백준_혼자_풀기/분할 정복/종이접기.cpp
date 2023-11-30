edfd8bbaffb5159464000de93132c49f01c7befa
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2023-11-30 14:51:54 +0900

    다시 풀기

--- JINSEOP/백준_혼자_풀기/분할 정복/종이접기.cpp
+++ JINSEOP/백준_혼자_풀기/분할 정복/종이접기.cpp
@@ -1,49 +1,44 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int tc = 0;
 string target = "";
-bool fold_paper(int start, int end) {
-	if (start >= end)
-		return true;
-  // 이 경우는, 끝까지 모든 면을 다 탐색한 경우
-
-	int left = start, right = end;
-	while (left < right) {
-		if (target[left] == target[right])
-			return false;
-    // 우선 각 면을 탐색할 때마다, 반대편은 반드시 다른 모양이어야 한다.
-    // 왜? 제대로 접혔다면 한 쪽이 IN이라면 겹치는 면은 반드시 OUT 일 것
-		left++; right--;
-	}
-
-	return fold_paper(start, right - 1);
-  // 재귀를 하는 이유는 각 면도 올바르게 접혔는지 확인하기 위함이다.
-  // 예를 들어, 3번 접었다면 총 7개의 접히는 구간이 나오게 될 텐데
-  // 처음 7개를 확인한 뒤, 절반인 3개를 확인해야만 한다.
-  // 절반인 3개를 확인했을 때도 올바르게 001 / 100과 같이 겹쳐있어야 하기 때문이다.
-  // 그래서 이 문제는 분할 정복 문제!
+void optimize() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 }
 
-int main() {
-	cin >> tc;
-	for (int i = 0; i < tc; i++) {
-		cin >> target;
-		// 핵심은 접히는 부분끼리는 반대 모양이어야 한다는 것!
+bool fold_paper(int left, int right) {
+    if (left >= right)
+        return true;
+    
+    int mid = (left + right) / 2;
+    for (int i = 0; i < (mid - left); i++) {
+        if (target[left + i] == target[right - i])
+            return false;
+    }
 
-		if (target.size() == 1) {
-			cout << "YES" << '\n';
-			continue; // 하나 짜리는 그냥 접을 수 있음
-		}
+    return fold_paper(left, mid - 1);
+}
 
-		if (target.size() % 2 == 0) {
-			cout << "NO" << '\n';
-			continue; // 짝수는 그냥 못 접음
-		}
+int main(){
+    optimize();
+    cin >> tc;
+    for (int i = 0; i < tc; i++) {
+        cin >> target;
 
-		if (fold_paper(0, target.size() - 1))
-			cout << "YES" << '\n';
-		else
-			cout << "NO" << '\n';
-	}
+        if (target.size() == 1) {
+            cout << "YES" << '\n';
+            continue;
+        }
+
+        if (fold_paper(0, target.size() - 1))
+            cout << "YES" << '\n';
+        else
+            cout << "NO" << "\n";
+    }
+
+    return 0;
 }
