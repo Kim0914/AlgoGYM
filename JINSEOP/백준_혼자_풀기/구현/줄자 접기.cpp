2dd31758ffa1defba2443a9a811446fd133aa4f7
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2023-12-30 14:25:52 +0900

    왜 안되지?

--- JINSEOP/백준_혼자_풀기/구현/줄자 접기.cpp
+++ JINSEOP/백준_혼자_풀기/구현/줄자 접기.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 double ruler_length = 0;
@@ -14,41 +15,59 @@ int main() {
 
 	// 빨간 걸 먼저 접자. 접을 때 점 위치 모두 옮기기
 	double fold_point = (red_dots.first + red_dots.second) / 2;
+	double left = 0, right = ruler_length;
 	fold_state[0] = true;
+
+	blue_dots.first = fold_point + fabs(fold_point - blue_dots.first);
+	blue_dots.second = fold_point + fabs(fold_point - blue_dots.second);
+
+	yellow_dots.first = fold_point + fabs(fold_point - yellow_dots.first);
+	yellow_dots.second = fold_point + fabs(fold_point - yellow_dots.second);
 
 	// 접힌 길이는 어떻게 계산?
 	// 둘 중 더 큰 쪽이 접힌 길이가 된다.
-	ruler_length = (fold_point, ruler_length - fold_point);
-	
-	if (abs(fold_point - blue_dots.first) == abs(fold_point - blue_dots.second))
-		fold_state[1] = true;
-	// 파란 게 접혔는 지 확인
-	if (abs(fold_point - yellow_dots.first) == abs(fold_point - yellow_dots.second))
-		fold_state[2] = true;
-	// 노란 게 접혔는 지 확인
+	if (fold_point < (left + right) / 2)
+		left = fold_point;
+	else
+		right = fold_point;
 
-	cout << fold_point << ' ' << ruler_length << '\n';
+	
+	// 중요한 것은, 접었을 때 점 위치를 반대편으로 옮겨주어야 한다.
+	if (blue_dots.first == blue_dots.second)
+		fold_state[1] = true;
+	// 파란 게 맞았는지 확인
+	if (yellow_dots.first == yellow_dots.second)
+		fold_state[2] = true;
+	// 노란 게 맞았는지 확인
+
 	if (!fold_state[1]) {
 	// 파란 게 아직 안접혔으면
 		fold_point = (blue_dots.first + blue_dots.second) / 2;
 		fold_state[1] = true;
 
-		if (abs(fold_point - yellow_dots.first) == abs(fold_point - yellow_dots.second))
+		yellow_dots.first = fold_point + fabs(fold_point - yellow_dots.first);
+		yellow_dots.second = fold_point + fabs(fold_point - yellow_dots.second);
+
+		if (yellow_dots.first == yellow_dots.second)
 			fold_state[2] = true;
 
-		ruler_length = (fold_point, ruler_length - fold_point);
+		if (fold_point < (left + right) / 2)
+			left = fold_point;
+		else
+			right = fold_point;
 	}
-
-	cout << fold_point << ' ' << ruler_length << '\n';
 
 	if (!fold_state[2]) {
 	// 노란 게 아직 안접혔으면
 		fold_point = (yellow_dots.first + yellow_dots.second) / 2;
 		fold_state[2] = true;
 
-		ruler_length = (fold_point, ruler_length - fold_point);
+		if (fold_point < (left + right) / 2)
+			left = fold_point;
+		else
+			right = fold_point;
 	}
 
-	cout << fold_point << ' ' << ruler_length << '\n';
+	cout << fabs(left - right);
 	return 0;
 }
