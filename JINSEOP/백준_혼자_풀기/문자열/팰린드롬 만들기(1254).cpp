32e0cc5335ba1572f09875e41def1cb5cc8dcdb1
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2024-01-12 15:34:42 +0900

    팰린드롬 탐색의 다양한 구현

---
+++ JINSEOP/백준_혼자_풀기/문자열/팰린드롬 만들기(1254).cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+string target = "";
+bool check_palindrome(int start_idx) {
+	for (int i = start_idx; i < target.size(); i++) {
+		if (target[i] != target[target.size() - 1 - (i - start_idx)])
+			return false;
+		// start_idx 기준 제일 앞과 제일 뒤부터 팰린드롬 여부 검사
+	}
+
+	// 마지막 한글자만 남는 경우는 어차피 true를 반환하게 됨.
+	return true;
+}
+
+int main() {
+	cin >> target;
+
+	int answer = target.size();
+	bool is_palindrome = false;
+	for (int i = 0; i < target.size(); i++) {
+		if (check_palindrome(i))
+			is_palindrome = true;
+
+		if (is_palindrome) {
+			cout << target.size() + i;
+			break;
+		}
+	}
+
+	return 0; 
+}
