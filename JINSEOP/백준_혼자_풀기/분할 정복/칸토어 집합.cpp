1adda5b13afd6398544b27437aff58a57c9886d2
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2024-01-24 13:58:03 +0900

    이게 왜 틀린코드지

---
+++ JINSEOP/백준_혼자_풀기/분할 정복/칸토어 집합.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int num = 0;
+void do_cantore(string target) {
+	if (target.size() == 1) {
+		cout << target;
+		return;
+	}
+
+	string temp = "";
+	for (int i = 0; i < target.size() / 3; i++)
+		temp += target[i];
+
+	do_cantore(temp);
+	for (int i = 0; i < target.size() / 3; i++)
+		cout << " ";
+	do_cantore(temp);
+}
+
+int main() {
+	while (cin >> num) {
+		string target = "";
+
+		for (int i = 0; i < pow(3, num); i++)
+			target += '-';
+
+		do_cantore(target);
+		cout << '\n';
+		return 0;
+	}
+}
