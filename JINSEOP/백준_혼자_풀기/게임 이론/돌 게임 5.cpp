c007bd030f269778f5efd420c247e8ed0aa6cd18
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2024-02-09 13:42:45 +0900

    범위가 넓다, long long 사용

---
+++ JINSEOP/백준_혼자_풀기/게임 이론/돌 게임 5.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+#define ll long long
+
+int main() {
+    ll n = 0;
+    cin >> n;
+
+    if (n % 2)
+        cout << "SK";
+    else
+        cout << "CY";
+    return 0;
+}
