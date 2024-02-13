9504dbec9a5ab7db18aa9f7680d09b4fc50a456a
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2024-02-13 23:46:43 +0900

    도전 중

---
+++ JINSEOP/프로그래머스_혼자_풀기/카드 짝 맞추기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+using namespace std;
+void make_permutation() {
+
+}
+
+int solution(vector<vector<int>> board, int r, int c) {
+    int answer = 0;
+
+
+    return answer;
+}
+
+int main() {
+    solution({ {1,0,0,3}, {2,0,0,0}, {0,0,0,2}, {3,0,1,0} }, 1, 0);
+    return 0;
+}
