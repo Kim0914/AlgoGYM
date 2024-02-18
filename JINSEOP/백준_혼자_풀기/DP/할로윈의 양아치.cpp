ce03871fcdd906778d5f52389d2066ae2f6a2946
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2024-02-18 20:57:11 +0900

    Union find 부분을 잘못 생각했음.

--- JINSEOP/백준_혼자_풀기/DP/할로윈의 양아치.cpp
+++ JINSEOP/백준_혼자_풀기/DP/할로윈의 양아치.cpp
@@ -4,20 +4,21 @@
 using namespace std;
 #define pii pair<int, int>
 
-unordered_map<int, int> duplication_preventer;
 vector<pii> group_store;
 pii groups[30001];
+int dp[30001][3001], popularity[30001];
 int num = 0, edges = 0, limit = 0, from = 0, to = 0;
-int dp[30001][3001];
 void optimize() {
-	cin.tie(NULL); 
-	cout.tie(NULL); 
+	cin.tie(NULL);
+	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 }
 
 void init_groups() {
-	for (int i = 1; i <= num; i++)
+	for (int i = 1; i <= num; i++) {
 		groups[i].first = i;
+		popularity[i] = 1;
+	}
 }
 
 int find_parent(int node) {
@@ -35,17 +36,28 @@ void do_union(int node_x, int node_y) {
 		groups[node_y].first = node_x;
 		groups[node_x].second += groups[node_y].second;
 		// 사탕 수를 더해서 저장해준다.
+		popularity[node_x] += popularity[node_y];
 	}
 	else {
 		groups[node_x].first = node_y;
 		groups[node_y].second += groups[node_x].second;
+		popularity[node_y] += popularity[node_x];
 	}
+}
+
+bool check_cycle(int node_x, int node_y) {
+	node_x = find_parent(node_x);
+	node_y = find_parent(node_y);
+
+	if (node_x == node_y)
+		return true;
+	return false;
 }
 
 int main() {
 	optimize();
 	cin >> num >> edges >> limit;
-	
+
 	init_groups();
 	for (int i = 1; i <= num; i++)
 		cin >> groups[i].second;
@@ -53,35 +65,30 @@ int main() {
 
 	for (int i = 0; i < edges; i++) {
 		cin >> from >> to;
-		do_union(from, to);
-		// 사이클 체크가 필요할까?
+		if(!check_cycle(from, to))
+			do_union(from, to);
 	}
 
 	for (int i = 1; i <= num; i++) {
 		int target = groups[i].first;
 		int target_candy = groups[i].second;
 
-		if (duplication_preventer[target]) {
-			duplication_preventer[target]++;
-			continue;
-		}
-
-		group_store.push_back({ target, target_candy });
-		duplication_preventer[target] = true;
+		if (target == i) // root인 경우 배열에 저장
+			group_store.push_back({ target, target_candy });
 	}
 
-	for (int i = 1; i <= group_store.size(); i++) {
-		for (int j = 1; j <= limit; j++) {
-			int curr_kids = duplication_preventer[group_store[i - 1].first];
-			int curr_value = group_store[i - 1].second;
+	for (int i = 0; i < group_store.size(); i++) {
+		for (int j = 0; j < limit; j++) {
+			int curr_kids = popularity[group_store[i].first];
+			int curr_value = group_store[i].second;
 
-			if (j - curr_kids > 0)
-				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - curr_kids] + curr_value);
+			if (j - curr_kids >= 0)
+				dp[i + 1][j] = max(dp[i][j], dp[i][j - curr_kids] + curr_value);
 			else
-				dp[i][j] = dp[i - 1][j];
+				dp[i + 1][j] = dp[i][j];
 		}
 	}
 
-	cout << dp[group_store.size()][limit];
+	cout << dp[group_store.size()][limit - 1];
 	return 0;
 }
