028a2c02792d5ab911d1b55fb85e0f0d64df4df6
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2024-02-22 18:52:29 +0900

    DP

--- JINSEOP/프로그래머스_혼자_풀기/카운트다운.cpp
+++ JINSEOP/프로그래머스_혼자_풀기/카운트다운.cpp
@@ -61,3 +61,80 @@ int main() {
     solution(58);
 	return 0;
 }
+
+//////////////////////////// 아래는 분기가 많아진 DP(튜닝)
+
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+#define pii pair<int, int>
+
+pii dp[100001];
+void fill_dp(int target) {
+    if (dp[target - 60].first == dp[target - 50].first) {
+        dp[target].first = dp[target - 50].first + 1;
+        dp[target].second = max(dp[target - 60].second, dp[target - 50].second + 1);
+        // 트리플을 하거나, 불을 던져 불 + 1을 하거나
+    }
+    else if (dp[target - 60].first < dp[target - 50].first) {
+        dp[target].first = dp[target - 60].first + 1;
+        dp[target].second = dp[target - 60].second;
+    }
+    else {
+        dp[target].first = dp[target - 50].first + 1;
+        dp[target].second = dp[target - 50].second + 1;
+    }
+}
+
+vector<int> solution(int target) {
+    vector<int> answer;
+
+    for (int i = 1; i <= target; i++) {
+        // 1. 싱글 1회, 불 1회로 끝나는 경우
+        if (i == 50 || i <= 20) {
+            dp[i].first = 1;
+            dp[i].second = 1;
+        }
+        // 2. 60이하일 때, 3의 배수인 경우
+        else if (i <= 60 && i % 3 == 0) {
+            dp[i].first = 1;
+            dp[i].second = 0;
+        }
+        // 3. 40이하일 때, 2의 배수인 경우
+        else if (i <= 40 && i % 2 == 0) {
+            dp[i].first = 1;
+            dp[i].second = 0;
+        }
+        // 4. 51 ~ 70일 때 불 + 싱글
+        else if (i > 50 && i <= 70) {
+            dp[i].first = 2;
+            dp[i].second = 2;
+        }
+        // 위의 경우에 포함되지 않는 경우
+        else if (i < 70) {
+            // 40보다 작은 경우는 싱글 + 싱글
+            if (i < 40) {
+                dp[i].first = 2;
+                dp[i].second = 2;
+            }
+            // 아니라면 싱글 + 더블 혹은 싱글 + 트리플
+            else {
+                dp[i].first = 2;
+                dp[i].second = 1;
+            }
+        }
+        // 남은 경우는 모두 dp로 해결
+        else
+            fill_dp(i);
+    }
+
+    answer.push_back(dp[target].first);
+    answer.push_back(dp[target].second);
+    return answer;
+}
+
+int main() {
+    solution(58);
+	return 0;
+}
