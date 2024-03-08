9137736c5006b9ed843ea80895f9d994cdf8fcf5
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2024-03-08 15:32:04 +0900

    SQL 감 잃지 않기

--- JINSEOP/프로그래머스_SQL/업그레이드 할 수 없는 아이템 구하기.sql
+++ JINSEOP/프로그래머스_SQL/업그레이드 할 수 없는 아이템 구하기.sql
@@ -4,3 +4,15 @@ WHERE ITEM_ID NOT IN (SELECT PARENT_ITEM_ID
                       FROM ITEM_TREE
                       WHERE PARENT_ITEM_ID IS NOT NULL)
 ORDER BY ITEM_ID DESC
+
+#####################################################
+
+SELECT ii.ITEM_ID, ITEM_NAME, RARITY
+FROM ITEM_INFO as ii LEFT JOIN ITEM_TREE as it 
+               ON ii.ITEM_ID = it.PARENT_ITEM_ID
+WHERE it.PARENT_ITEM_ID IS NULL
+ORDER BY ii.ITEM_ID DESC
+
+# LEFT JOIN을 통해서 푼 풀이
+# LEFT JOIN을 하게 되면, ITEM_ID 중 PARENT_ITEM_ID가 없는 row는 NULL이 된다.
+# 따라서 IS NULL을 하게 되면, PARENT_ITEM_ID에 없는, 즉 업그레이드 불가능한 아이템이 나옴
