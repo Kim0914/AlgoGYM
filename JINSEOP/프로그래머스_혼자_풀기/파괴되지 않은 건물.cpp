562ba8fb35d1503b8d6bd30bd887944a6b4bc3d2
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2024-03-11 02:15:32 +0900

    무슨 이런 미친 문제가..

--- JINSEOP/프로그래머스_혼자_풀기/파괴되지 않은 건물.cpp
+++ JINSEOP/프로그래머스_혼자_풀기/파괴되지 않은 건물.cpp
@@ -3,30 +3,43 @@
 #include <vector>
 using namespace std;
 
+int dp[1001][1001];
+void input_coefficient(int x1, int x2, int y1, int y2, int coef) {
+    dp[x1][y1] += coef;
+    dp[x2 + 1][y1] -= coef;
+    dp[x1][y2 + 1] -= coef;
+    dp[x2 + 1][y2 + 1] += coef;
+}
+
 int solution(vector<vector<int>> board, vector<vector<int>> skill) {
     int answer = 0;
 
-    // 그냥 구현할 경우 반드시 시간초과 발생
-    for (auto infos : skill) {
-        if (infos[0] == 1) {
-        // 지형 파괴
-            for (int i = infos[1]; i <= infos[3]; i++)
-                for (int j = infos[2]; j <= infos[4]; j++)
-                    board[i][j] -= infos[5];
-        }
-        else {
-        // 지형 회복
-            for (int i = infos[1]; i <= infos[3]; i++)
-                for (int j = infos[2]; j <= infos[4]; j++)
-                    board[i][j] += infos[5];
-        }
-    }
+    for (auto info : skill) {
+        int x1 = info[1];
+        int y1 = info[2];
+        int x2 = info[3];
+        int y2 = info[4];
+
+        if (info[0] == 1)
+            input_coefficient(x1, x2, y1, y2, info[5] * -1);
+        else
+            input_coefficient(x1, x2, y1, y2, info[5]);
+    } // 여기서 dp 배열에 누적합 계수를 모두 저장한다.
+
+    for (int i = 0; i <= board.size(); i++)
+        for (int j = 0; j < board[0].size(); j++)
+            dp[i][j + 1] += dp[i][j];
+
+    for (int j = 0; j <= board[0].size(); j++)
+        for (int i = 0; i < board.size(); i++)
+            dp[i + 1][j] += dp[i][j];
+    // 위의 두 for문이 누적합 계수를 만드는 과정
 
     for (int i = 0; i < board.size(); i++)
         for (int j = 0; j < board[0].size(); j++)
-            if (board[i][j] > 0)
+            if (board[i][j] + dp[i][j] > 0)
                 answer++;
-    // 살아있는 지형 카운트
+    // 최종적으로 남은 계수들을 더했을 때 살아있는 지형 카운트
 
     return answer;
 }
