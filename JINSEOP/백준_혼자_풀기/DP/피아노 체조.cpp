b46d98fbf39d2ba2c4b1990cf142e0b1a63911ed
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2024-03-11 15:11:14 +0900

    최적화 코드가 들어가야 시간초과가 안나네;

--- JINSEOP/백준_혼자_풀기/DP/피아노 체조.cpp
+++ JINSEOP/백준_혼자_풀기/DP/피아노 체조.cpp
@@ -2,19 +2,24 @@
 #include <string>
 #include <vector>
 using namespace std;
-#define MAX 99999999
 
 int num = 0, q_num = 0, from = 0, to = 0;
 int composes[100002], dp[100002];
-int main() {
-	cin >> num;
-	for (int i = 1; i <= num; i++)
-		cin >> composes[i];
-	composes[num + 1] = MAX;
+void optimize() {
+	ios_base::sync_with_stdio(false); 
+	cin.tie(NULL); 
+	cout.tie(NULL);
+}
 
+int main() {
+	optimize();
+
+	cin >> num;
 	for (int i = 1; i <= num; i++) {
+		cin >> composes[i];
+
 		if (composes[i] < composes[i - 1])
-			dp[i] = dp[i-1] + 1;
+			dp[i] = dp[i - 1] + 1;
 		else
 			dp[i] = dp[i - 1];
 	}
