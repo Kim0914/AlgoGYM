75007ba2a06b28834ee1dc89e2668f863932e9e7
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2024-03-28 14:17:27 +0900

    IFNULL 사용법

--- JINSEOP/프로그래머스_SQL/대장균들의 자식의 수 구하기.sql
+++ JINSEOP/프로그래머스_SQL/대장균들의 자식의 수 구하기.sql
@@ -4,3 +4,11 @@ FROM ECOLI_DATA ed LEFT OUTER JOIN(SELECT PARENT_ID, COUNT(PARENT_ID) AS CHILD_C
                                 FROM ECOLI_DATA
                                 GROUP BY PARENT_ID) sub_a
                     ON ed.ID = sub_a.PARENT_ID
+
+#################### 이렇게 IFNULL도 쓸 수 있다.
+  
+SELECT ID, IFNULL(CHILD_COUNT, 0) AS CHILD_COUNT
+FROM ECOLI_DATA ed LEFT OUTER JOIN(SELECT PARENT_ID, COUNT(PARENT_ID) AS CHILD_COUNT
+                                FROM ECOLI_DATA
+                                GROUP BY PARENT_ID) sub_a
+                    ON ed.ID = sub_a.PARENT_ID
