e69a8c8723997adbdc73d07a5b25b7d0a5d5ee46
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2024-04-05 13:01:14 +0900

    DP인데 이제 DP같지 않은..

--- JINSEOP/백준_혼자_풀기/DP/문자열 판별.cpp
+++ JINSEOP/백준_혼자_풀기/DP/문자열 판별.cpp
@@ -18,11 +18,14 @@ int main(){
 
 	for (int i = 0; i < dest.size(); i++) {
 		if (dp[i] || i == 0) {
+		// 만들어야 하는 문자열이거나 처음 시작일 때 진행
 			int start = i;
 
 			for (int j = 0; j < src_vec.size(); j++) {
 				bool can_make = true;
 				for (int k = 0; k < src_vec[j].size(); k++) {
+				// 모든 단어를 순회하며 만들 수 있는지 체크한다.
+				// 이 과정에서 최대 100 * 100 * 100 = 100만 밖에 소요 안됨
 					if (src_vec[j][k] != dest[start + k]) {
 						can_make = false;
 						break; // 문자가 다르면 즉시 중지
@@ -33,7 +36,7 @@ int main(){
 					dp[start + src_vec[j].size()] = true;
 				// 문자열을 만약 만들었으면 그 다음칸에 표시 해야함
 			}
-		} // 만들어야 하는 문자열이거나 처음 시작일 때 진행
+		}
 	}
 
 	cout << dp[dest.size()];
