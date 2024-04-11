bb826c108ad98690ae424fdba4f65660c08d9adf
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2024-04-11 12:12:17 +0900

    BFS를 응용하자

--- JINSEOP/백준_혼자_풀기/그래프 탐색/점프 게임.cpp
+++ JINSEOP/백준_혼자_풀기/그래프 탐색/점프 게임.cpp
@@ -4,73 +4,48 @@ using namespace std;
 #define OPTIMIZE cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(false);
 #define piic pair<pair<int, int>, char>
 
-bool left_line[100001], right_line[100001], left_visit[100001], right_visit[100001];
+// 점프하려면 20만까지는 갈 수 있어야 함
+bool lines[2][200001], line_visit[2][200001];
 int num = 0, coef = 0;
-void init_lines() {
-	for (int i = 0; i <= 100000; i++) {
-		left_line[i] = 1;
-		right_line[i] = 1;
-	}
+void init_lines(){
+	for (int i = 0; i < 2; i++)
+		for (int j = 0; j <= 200000; j++)
+			lines[i][j] = 1;
 }
 
 bool bfs() {
 	queue<piic> bfs_q;
-	bfs_q.push({ { 1, 1}, 'L' }); // 왼쪽 줄 1번 칸
-	left_visit[1] = true;
+	bfs_q.push({ { 1, 1 }, 0 }); // 0 : 왼쪽 줄, 1 : 오른쪽 줄
+	line_visit[1][1] = true;
 
-	int prev_time = 1;
 	while (!bfs_q.empty()) {
 		int curr_cell = bfs_q.front().first.first;
 		int curr_time = bfs_q.front().first.second;
-		char curr_line = bfs_q.front().second;
+		int curr_line = bfs_q.front().second;
 		bfs_q.pop();
 
-		if (curr_time >= num)
-			break;
-		if (curr_time == prev_time + 1) {
-			left_line[prev_time] = 0;
-			right_line[prev_time] = 0;
-			prev_time++;
+		if (curr_cell >= num)
+			return true;
+
+		if (lines[curr_line][curr_cell + 1] && !line_visit[curr_line][curr_cell + 1]) {
+			bfs_q.push({ {curr_cell + 1, curr_time + 1}, curr_line });
+			line_visit[curr_line][curr_cell + 1] = true;
 		}
-
-		if (curr_line == 'L') {
-			if (left_line[curr_cell + 1] && (curr_cell + 1 <= 100000)) {
-				if (curr_cell + 1 > num)
-					return true;
-
-				bfs_q.push({ {curr_cell + 1, curr_time + 1}, 'L' });
-				left_visit[curr_cell + 1] = true;
-			}
-			if (left_line[curr_cell - 1] && curr_cell >= 2 && !left_visit[curr_cell - 1] && (curr_cell - 1 != prev_time)) {
-				bfs_q.push({ { curr_cell - 1, curr_time + 1}, 'L' });
-				left_visit[curr_cell - 1] = true;
-			}
-			if (right_line[curr_cell + coef] && (curr_cell + coef <= 100000)) {
-				if (curr_cell + coef > num)
-					return true;
-
-				bfs_q.push({ { curr_cell + coef, curr_time + 1}, 'R' });
-				right_visit[curr_cell + coef] = true;
+		if (lines[curr_line][curr_cell - 1] && !line_visit[curr_line][curr_cell - 1] && (curr_cell - 1) > curr_time) {
+			// 시간에 대한 제한이 들어가는 것은, 발판이 사라지기 때문 
+			bfs_q.push({ { curr_cell - 1, curr_time + 1}, curr_line });
+			line_visit[curr_line][curr_cell - 1] = true;
+		}
+		if (curr_line == 0) { // 왼쪽
+			if (lines[1][curr_cell + coef] && !line_visit[1][curr_cell + coef]) {
+				bfs_q.push({ { curr_cell + coef, curr_time + 1}, 1 });
+				line_visit[1][curr_cell + coef] = true;
 			}
 		}
-		else {
-			if (right_line[curr_cell + 1] && (curr_cell + 1 <= 100000)) {
-				if (curr_cell + 1 > num)
-					return true;
-
-				bfs_q.push({ {curr_cell + 1, curr_time + 1}, 'R' });
-				right_visit[curr_cell + 1] = true;
-			}
-			if (right_line[curr_cell - 1] && curr_cell >= 2 && !right_visit[curr_cell - 1] && (curr_cell - 1 != prev_time)) {
-				bfs_q.push({ {curr_cell - 1, curr_time + 1}, 'R' });
-				right_visit[curr_cell - 1] = true;
-			}
-			if (left_line[curr_cell + coef] && (curr_cell + coef <= 100000)) {
-				if (curr_cell + coef > num)
-					return true;
-
-				bfs_q.push({ {curr_cell + coef, curr_time + 1}, 'L' });
-				left_visit[curr_cell + coef] = true;
+		if (curr_line == 1) { // 오른쪽
+			if (lines[0][curr_cell + coef] && !line_visit[0][curr_cell + coef]) {
+				bfs_q.push({ { curr_cell + coef, curr_time + 1}, 0 });
+				line_visit[0][curr_cell + coef] = true;
 			}
 		}
 	}
@@ -80,14 +55,14 @@ bool bfs() {
 
 int main() {
 	OPTIMIZE;
+	init_lines();
 	cin >> num >> coef;
 
-	init_lines();
 	string temp_l = "", temp_r = "";
 	cin >> temp_l >> temp_r;
 	for (int i = 1; i <= num; i++) {
-		left_line[i] = temp_l[i - 1] - '0';
-		right_line[i] = temp_r[i - 1] - '0';
+		lines[0][i] = temp_l[i - 1] - '0';
+		lines[1][i] = temp_r[i - 1] - '0';
 	}
 
 	bool can_clear = bfs();
