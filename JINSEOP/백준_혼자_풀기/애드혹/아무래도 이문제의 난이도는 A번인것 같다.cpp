d4d23826a4dfc4da661b930e09e27d1443b73e40
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2024-04-27 14:42:41 +0900

    어이가 없네

---
+++ JINSEOP/백준_혼자_풀기/애드혹/아무래도 이문제의 난이도는 A번인것 같다.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+#define OPTIMIZE ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+int tc = 0, a = 0, b = 0;
+int main() {
+	OPTIMIZE;
+	cin >> tc;
+
+	while (tc--) {
+		cin >> a >> b;
+		cout << "yes" << '\n';
+	}
+
+	return 0;
+}
