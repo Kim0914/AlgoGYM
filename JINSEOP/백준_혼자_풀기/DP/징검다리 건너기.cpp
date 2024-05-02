30760854207702d1b521ebb195addd727ec5633e
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2024-05-02 11:45:26 +0900

    조금 머리아픈 DP

---
+++ JINSEOP/백준_혼자_풀기/DP/징검다리 건너기.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+#define MAX 1000001
+#define INF 10000000000
+typedef long long ll;
+
+int num = 0;
+ll stones[5000], dp[5000];
+void init_dp() {
+	for (int i = 0; i < num; i++)
+		dp[i] = MAX;
+}
+
+void calculate_dp() {
+	dp[0] = 0;
+	dp[1] = abs(stones[0] - stones[1]) + 1;
+
+	for (int i = 2; i < num; i++) {
+		for (int j = 0; j < i; j++) {
+			ll each_fatigue = (i - j) * (1 + abs(stones[i] - stones[j]));
+			dp[i] = min(dp[i], max(dp[j], each_fatigue));
+		}
+	}
+}
+
+void print_dp() {
+	for (int i = 0; i < num; i++) {
+		cout << dp[i] << " ";
+	}
+}
+
+bool check_stone(ll target_fatigue) {
+	for (int i = 0; i < num; i++) {
+		for (int j = i + 1; j < num; j++) {
+			ll each_fatigue = (j - i) * (1 + abs(stones[i] - stones[j]));
+			
+			if (each_fatigue > target_fatigue)
+				return false;
+		}
+	}
+
+	return true;
+}
+
+int main() {
+	cin >> num;
+	for (int i = 0; i < num; i++)
+		cin >> stones[i];
+	// i -> j 번째 돌로 이동하는 경우
+	// (j - i) * (1 + |A[i] - A[j]|) 만큼 힘을 쓴다.
+	// 모든 i -> j가 K 이하여야 한다는 말!
+
+	init_dp();
+	calculate_dp();
+	print_dp();
+
+	//cout << dp[num - 1];
+	return 0;
+}
