eba8c77d3d458f77bdc2921873c2d95ce1b32b99
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2024-05-05 11:53:12 +0900

    C++도 정규식 표현이 가능하다

---
+++ JINSEOP/백준_혼자_풀기/문자열/잠수함 식별.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <regex>
+using namespace std;
+
+string target = "";
+int main() {
+	cin >> target;
+
+	regex re("((100+1+)|(01)+)+");
+	if (regex_match(target, re))
+		cout << "SUBMARINE";
+	else
+		cout << "NOISE";
+
+	return 0;
+}
