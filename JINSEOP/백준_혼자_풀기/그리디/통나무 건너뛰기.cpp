1eedd30c45c1f3be322c116c685ecd843b0d15bb
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2024-05-22 21:11:00 +0900

    그리디 그리디

--- JINSEOP/백준_혼자_풀기/그리디/통나무 건너뛰기.cpp
+++ JINSEOP/백준_혼자_풀기/그리디/통나무 건너뛰기.cpp
@@ -26,6 +26,10 @@ int main()
 		for (int i = 3; i < num; i += 2)
 			answer = max(answer, height_info[i] - height_info[i - 2]);
 
+		// 그리디 문제
+		// 정렬을 한 뒤, 작은 숫자부터 앞 뒤 앞 뒤 앞 뒤 집어넣는다.
+		// 그렇게 되면 2칸 씩 건너뛰어서 배열을 훑는 것과 같음
+		// 밑에 한번 더 훑는 것은 홀수칸 배열을 훑는 것
 		cout << answer << '\n';
 	}
 
